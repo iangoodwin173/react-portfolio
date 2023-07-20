5467f6875092252f406c6171c3c4fb2df5885ec3
Ian Goodwin <iangoodwin173@gmail.com>
2023-07-20 17:17:22 -0400

    file structure and third link

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="App-footer">
+      <h2>Find my links here:</h2>
+      <p>
+      <a href="https://github.com/iangoodwin173/" target="_blank" className='footer-link'>GitHub</a> || <a href="https://www.linkedin.com/in/ian-goodwin-0487b6187/" target="_blank" className='footer-link'>LinkedIn</a> || <a href="https://www.tiktok.com/@kiltedphoenix" target="_blank" className='footer-link'>TikTok</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
