8aa587ce7439be2b3a50f974b74e9ec0d48c786c
Ian Goodwin <iangoodwin173@gmail.com>
2023-07-25 21:24:08 -0400

    added final project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import workdayPicture from './assets/images/Workday-Project.jpg';
 import quizPicture from './assets/images/Quiz-Picture.png';
+import geoPairing from './assets/images/geopairing.png';
 
 
 
@@ -16,6 +17,11 @@ function Projects() {
       <a href='https://iangoodwin173.github.io/coding-quiz/'>
         <img className='project-images' src={quizPicture} alt='quiz project'></img>
         <a href='https://github.com/iangoodwin173/coding-quiz'>Repository</a>
+      </a>
+
+      <a href='https://geo-pairing-2def7ca5b56b.herokuapp.com/'>
+        <img className='project-images' src={geoPairing} alt='quiz project'></img>
+        <a href='https://github.com/iangoodwin173/geo-pairing'>Repository</a>
       </a>
       <p>More projects coming soon</p>
     </div>
